2e76ed4f04ecfaacc0d08f8e34404692b8fc900e
Robert Farb <farb.robert@gmail.com>
2018-11-13 15:13:34 -0800

    Change navbar a tags to Link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
@@ -6,7 +7,9 @@ class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
           <div className="container">
-            <a className="navbar-brand" href="landing.html">OutageManager</a>
+            <Link className="navbar-brand" to="/">
+              OutageManager
+            </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -14,16 +17,22 @@ class Navbar extends Component {
         <div className="collapse navbar-collapse" id="mobile-nav">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="outages.html">Outages</a>
+                  <Link className="nav-link" to="/outages">
+                    Outages
+                  </Link>
                 </li>
               </ul>    
 
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="register.html">Sign Up</a>
+                  <Link className="nav-link" to="/register">
+                    Sign up
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="login.html">Login</a>
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
                 </li>
               </ul>
             </div>
