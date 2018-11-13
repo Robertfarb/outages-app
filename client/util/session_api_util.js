2fa61ffbaeb899b08e1305fb399bfc77c25b58eb
Robert Farb <farb.robert@gmail.com>
2018-11-12 23:08:48 -0800

    Add host, and session_api_util

---
+++ client/util/session_api_util.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import HOST from './host';
+
+export const fetchCurrentSession = token =>
+  axios.get(`${HOST}/api/users/current`, { headers: token });
+
+export const loginUser = user =>
+  axios.post(`${HOST}/api/users/login`, user);
+
+export const endSession = () => axios.get(`${HOST}/api/session/logout`);
