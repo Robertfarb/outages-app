67f96649be90de2e2bf4a62e821842fe6395f7bb
Robert Farb <farb.robert@gmail.com>
2018-11-13 22:00:32 -0800

    Add Outage updates to details card

--- client/src/components/outages/outage_detail_card.js
+++ client/src/components/outages/outage_detail_card.js
@@ -21,26 +21,46 @@ class OutageDetailCard extends Component {
         <h1>LOADING...</h1>
       )
     } else {
-      return (
-        <div className="card text-center">
+      return <div className="card text-center">
           <div className="card-header">
             Outage Type: {outage.outageType}
           </div>
           <div className="card-body">
-            <h4 className="card-title">Outage City: {outage.locationCity}</h4>
+            <h4 className="card-title">
+              Outage City: {outage.locationCity}
+            </h4>
             <p className="card-text">Outage Address: {outage.address}</p>
             <p className="card-text">Outage Zip Code: {outage.zipCode}</p>
             <p className="card-text">Outage Type: {outage.outageType}</p>
             <p className="card-text">Reason: {outage.reason}</p>
             <p className="card-text">Start Time: {outage.startTime}</p>
-            <p className="card-text">Start Time: {updates}</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <h2>Updates:</h2>{updates.map((update, idx) => (
+              <div className="m-4 card" key={idx}>
+                <h3>Update {idx + 1}</h3>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">
+                    <strong>Est. Restoration Time:</strong>{" "}
+                    {update.estimatedRestTime}
+                  </li>
+                  <li className="list-group-item">
+                    <strong>Crew Arrival Time:</strong>{" "}
+                    {update.crewArrivalTime}
+                  </li>
+                  <li className="list-group-item">
+                    <strong>Delay Reason:</strong> {update.delayReason}
+                  </li>
+                  <li className="list-group-item">
+                    <strong>Complete Restoration Time:</strong> {update.completeRestTime}
+                  </li>
+                </ul>
+              </div>
+            ))}
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
           </div>
-          <div className="card-footer text-muted">
-            2 days ago
-          </div>
-        </div>
-      )
+          <div className="card-footer text-muted">2 days ago</div>
+        </div>;
     }
   }
 }
